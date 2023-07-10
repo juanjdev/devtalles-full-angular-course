11a6b1066761e717f8a01512db732a203d9a1693
Juan J Blanco <juanjb.dev@gmail.com>
2023-07-09 22:00:47 -0500

    (feat) understanding basic concepts of Decorators

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -5,7 +5,8 @@ import './style.css'
 // import './topics/04-homework-types'
 // import './topics/05-basic-destructuring'
 // import './topics/08-classes'
-import './topics/09-generics'
+// import './topics/09-generics'
+import './topics/10-decorators'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo
