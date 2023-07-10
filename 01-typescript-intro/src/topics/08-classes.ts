6bce2a5b2a8226543cd852cbfae0802d10c45706
Juan J Blanco <juanjb.dev@gmail.com>
2023-07-09 21:36:28 -0500

    (feat) file for  composition vs inheritance understanding

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,23 +1,37 @@
 export class Person {
     
     constructor(
-        public name: string,
+        public fisrtName: string,
+        public lastName: string,
         private address: string = 'No Address'){
     }
 }
 
-export class Hero extends Person {
+// export class Hero extends Person {
+//     constructor(
+//         public alterEgo: string, 
+//         public age: number, 
+//         public realName: string
+//     ){
+//         super(realName, 'New York')
+//     }
+// } 
+
+
+export class Hero {
+
     constructor(
         public alterEgo: string, 
         public age: number, 
-        public realName: string
+        public realName: string,
+        public person: Person
     ){
-        super(realName, 'New York')
+        
     }
 } 
 
+const tony = new Person('Tony', 'Stark','New York');
 
-
-const ironman = new Hero('Ironman', 45, 'Tony');
+const ironman = new Hero('Ironman', 45, 'Tony', tony);
 
 console.log(ironman);
