2799e0e3f326841759b950880fee4e770a27a502
Juan J Blanco <juanjb.dev@gmail.com>
2023-07-09 20:48:14 -0500

    (feat) adding file for function destructuring

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,43 @@
+
+interface Product {
+    description: string; 
+    price: number; 
+
+}
+
+const phone : Product = {
+    description: 'Nokia A1', 
+    price: 150.0
+}
+
+const tablet: Product = {
+    description: 'Aipad Air', 
+    price: 250.0
+}
+
+interface TaxCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+function taxCalculate(options: TaxCalculationOptions) : [number, number]{
+    const {tax, products} = options;
+    let total = 0; 
+    products.forEach( ({price}) => {
+        total += price;
+    });
+    return [total, total * tax]
+}
+
+const shoppingCart = [phone, tablet]; 
+const tax = 0.15;
+
+const [total, taxResult] = taxCalculate({
+    products: shoppingCart, 
+    tax: tax
+})
+
+console.log('Total', total );
+console.log('Tax', taxResult)
+
+export {};
