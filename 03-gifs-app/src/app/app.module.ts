b769878af7ceb1c338c1dd721dd86388ad24a7f5
Juan J Blanco <juanjb.dev@gmail.com>
2023-07-11 23:30:05 -0500

    (feat) add files and folders for gifs app almost ready. it includes localstorage and multiple useful concepts!

--- 03-gifs-app/src/app/app.module.ts
+++ 03-gifs-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
@@ -7,15 +8,14 @@ import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     GifsModule,
     SharedModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
